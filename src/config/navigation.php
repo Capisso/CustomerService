1947a1f1b7d479ebc18cf04fa2bb6c1943e126da
Luke Strickland <luke@axxim.net>
2013-10-27 09:23:45 -0500

    Adding the ability to be active

--- src/config/navigation.php
+++ src/config/navigation.php
@@ -5,16 +5,19 @@ return array(
         array(
             'name' => 'Support',
             'icon' => 'icon-question-sign',
+            'active' => 'customer-service',
             'children' => array(
                 array(
                     'href' => 'Capisso\\CustomerService\\TicketController@index',
                     'name' => 'View Tickets',
-                    'icon' => 'icon-double-angle-right'
+                    'icon' => 'icon-double-angle-right',
+                    'active' => 'tickets'
                 ),
                 array(
                     'href' => 'Capisso\\CustomerService\\TicketController@create',
                     'name' => 'Create Ticket',
-                    'icon' => 'icon-double-angle-right'
+                    'icon' => 'icon-double-angle-right',
+                    'active' => 'tickets/create'
                 )
             )
         )
